43d8b6c04fac8ef107d39cb4784b9127e523e452
Cristobal Valenzuela Musura <cristobal.valenzuelamusura@cencosud.cl>
2023-06-01 01:52:54 -0400

    cambio en cli

--- titanic_fairy/cli.py
+++ titanic_fairy/cli.py
@@ -1,8 +1,9 @@
 """API interfaz de ejecución
 
 """
-
+from pathlib import Path
 import typer
+from titanic_fairy.helpers.check_data import check_table_
 
 # # from execslotting import logger
 
@@ -10,20 +11,37 @@ import typer
 
 """
 
-def main(): 
-    from titanic_fairy.helpers.check_data import APP as APP_CHECK
+app = typer.Typer()
 
-    APP = typer.Typer()
-    # chequea los datos y que el path a las tablas sea el correcto
-    APP.add_typer(APP_CHECK, name="check_tables")
-    # preprocesa los datos y los guarda en una tabla. 
-    # APP.add_typer(APP_PREPROCESS, name="preprocess") 
-    # # entrena un modelo y lo guarda en result/model
-    # APP.add_typer(APP_TRAIN, name="train_model")
-    # # genera graficos 
-    # APP.add_typer(APP_PREDICT, name="make_graphs")
-    # # predice 
-    # APP.add_typer(APP_DISTANCE, name="predict")
+
+PATH_OPTION = typer.Option(
+    str(Path("dataset/train.csv"))
+    , help="Ruta al archivo de datos para ser ingestado por el modelo"
+)
+
+@app.command()
+def check_table(path : Path = PATH_OPTION):
+    check = check_table_(path)
+    if check:
+        typer.echo("La tabla esta lsita para procesarse.")
+
+
+
+# def main(): 
+#     from titanic_fairy.helpers.check_data import APP as APP_CHECK
+
+#     APP = typer.Typer()
+#     # chequea los datos y que el path a las tablas sea el correcto
+#     APP.add_typer(APP_CHECK, name="check-tables")
+#     # preprocesa los datos y los guarda en una tabla. 
+#     # APP.add_typer(APP_PREPROCESS, name="preprocess") 
+#     # # entrena un modelo y lo guarda en result/model
+#     # APP.add_typer(APP_TRAIN, name="train_model")
+#     # # genera graficos 
+#     # APP.add_typer(APP_PREDICT, name="make_graphs")
+#     # # predice 
+#     # APP.add_typer(APP_DISTANCE, name="predict")
+#     APP()
 
 
     
--- titanic_fairy/helpers/check_data.py
+++ titanic_fairy/helpers/check_data.py
@@ -2,23 +2,14 @@
 
 from pathlib import Path
 import pandas as pd
-import typer
 from titanic_fairy.enums.titanic_fields import Fields
 
-APP = typer.Typer()
 
 
-PATH_OPTION = typer.Option(
-    str(Path("dataset/", "train.csv"))
-    , help="Ruta al archivo de datos para ser ingestado por el modelo"
-)
-
-
-@APP.command()
-def check_table(data_file: Path = PATH_OPTION):
+def check_table_(data_file: Path):
     """Chequea que las tablas en el path entregado esten parseadas adecuadamente
-    :param data_file: _description_, defaults to PATH_OPTION
-    :type data_file: Path, optional
+    :param data_file: _description_
+    :type data_file: Path
     """
     fields_names = [field.value for field in Fields]
     try:
@@ -30,7 +21,6 @@ def check_table(data_file: Path = PATH_OPTION):
         if col not in fields_names:
             raise ValueError("Datos poseen columna no esperada")
         flag = False
-    if flag:
-        print("Datos se encuentran en orden!")
+    return Flag
 
 
