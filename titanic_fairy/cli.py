6ad133b732bd6920c0f1e4e797806afe26c3bed5
Cristobal Valenzuela Musura <cristobal.valenzuelamusura@cencosud.cl>
2023-05-31 21:06:50 -0400

    ajskdsjak

--- titanic_fairy/cli.py
+++ titanic_fairy/cli.py
@@ -6,16 +6,42 @@ import typer
 
 # # from execslotting import logger
 
+"""API interfaz de ejecución de modelo para Titanic.
 
-# def main():
-#     """Main CLI interface."""
+"""
 
-#     from model.suggest. import APP as APP_MODEL
-#     from preprocessing.import APP as APP_PREPROCESS
-#     from metrics. import APP as APP_METRICS
+def main(): 
 
-#     APP = typer.Typer()
-#     APP.add_typer(APP_MODEL, name="train_model")
-#     APP.add_typer(APP_PREPROCESS, name="tables")
-#     APP.add_typer(APP_METRICS, name="metrics")
-#     APP()
+    from titanic_fairy.helpers.load_and_save import Load
+    from titanic_fairy.helpers.train_test_split import stratified_split
+    from titanic_fairy.metrics.graphics import show_heatmap, check_train_test_split
+    from titanic_fairy.preprocessing.preprocess import Preprocess
+    from titanic_fairy.model.train_model import build_model
+    from titanic_fairy.enums.titanic_fields import Fields, Preprocess
+    import numpy as np
+
+
+    # Cargamos los datos
+    titanic_raw_train = Load(train_path = "dataset/train.csv")
+
+    # Dividimos en entrenamiento y validacion
+    raw_train, raw_test = stratified_split(titanic_raw_train)
+
+    # Preprocesamos la data
+    preproc_train = Preprocess().fit_transform(raw_train)
+    preproc_test = Preprocess().fit_transform(raw_train)
+
+    # Separamos en datos y target
+    X_train = preproc_train.drop([Fields.Survived.value], axis=1)
+    y_train = preproc_train[Fields.Survived.value]
+
+    X_test = preproc_test.drop([Fields.Survived.value], axis=1)
+    y_test = preproc_test[Fields.Survived.value]
+
+    # Armamos el modelo
+    model = build_model(X_train, y_train)
+
+    # Generamos predicciones
+    model.predict(X_test)
+
+
